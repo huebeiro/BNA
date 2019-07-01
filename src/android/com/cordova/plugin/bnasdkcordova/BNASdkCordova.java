3c753f9353ba12de9bd70fe823a4e1dfbbd0cf16
Lucas <lucas.natal95@gmail.com>
2019-07-01 17:16:37 -0300

    4.3.1
    teste

--- src/android/com/cordova/plugin/bnasdkcordova/BNASdkCordova.java
+++ src/android/com/cordova/plugin/bnasdkcordova/BNASdkCordova.java
@@ -11,6 +11,21 @@ import android.util.Log;
 
 import org.json.JSONException;
 
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.os.Build;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.LOG;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import javax.security.auth.callback.Callback;
+
 public class BNASdkCordova extends CordovaPlugin {
 
     private static final String TAG = BNASdkCordova.class.getCanonicalName();
@@ -18,7 +33,14 @@ public class BNASdkCordova extends CordovaPlugin {
     private static final String BNA_STOP    = "stop";
 
     private CordovaInterface mCordovaInterface;
+    
+    CallbackContext context;
 
+     String [] permissions = { Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -30,14 +52,28 @@ public class BNASdkCordova extends CordovaPlugin {
         
         switch (action) {
             case BNA_GO:
+
                 try{
                     BnaSDK.create(this.mCordovaInterface.getContext().getApplicationContext());
                 }catch (Exception e){
                     Log.d("Description", e.toString());
                 }
-                BnaSDK.instance().go(this.mCordovaInterface.getContext().getApplicationContext());
-                callbackContext.success();
+
+                LOG.d(TAG, "We are entering execute");
+                context = callbackContext;
+                if(hasPermisssion())
+                {
+                    BnaSDK.instance().go(this.mCordovaInterface.getContext().getApplicationContext());
+                    PluginResult r = new PluginResult(PluginResult.Status.OK);
+                    context.sendPluginResult(r);
+                    callbackContext.success();
+                    return true;
+                }
+                else {
+                    PermissionHelper.requestPermissions(this, 0, permissions);
+                }
                 return true;
+
             case BNA_STOP:
                 BnaSDK.instance().stop(this.mCordovaInterface.getContext().getApplicationContext());
                 callbackContext.success();
@@ -47,4 +83,47 @@ public class BNASdkCordova extends CordovaPlugin {
         }
 
     }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException
+    {
+        PluginResult result;
+        //This is important if we're using Cordova without using Cordova, but we have the geolocation plugin installed
+        if(context != null) {
+            for (int r : grantResults) {
+                if (r == PackageManager.PERMISSION_DENIED) {
+                    LOG.d(TAG, "Permission Denied!");
+                    result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
+                    context.sendPluginResult(result);
+                    return;
+                }
+
+            }
+            BnaSDK.instance().go(this.mCordovaInterface.getContext().getApplicationContext());
+            result = new PluginResult(PluginResult.Status.OK);
+            context.sendPluginResult(result);
+        }
+    }
+
+    public boolean hasPermisssion() {
+        for(String p : permissions)
+        {
+            if(!PermissionHelper.hasPermission(this, p))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+     * We override this so that we can access the permissions variable, which no longer exists in
+     * the parent class, since we can't initialize it reliably in the constructor!
+     */
+
+    public void requestPermissions(int requestCode)
+    {
+        PermissionHelper.requestPermissions(this, requestCode, permissions);
+    }
+
 }
